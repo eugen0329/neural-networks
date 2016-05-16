f5829346c9c55d4e4921af844bcba58d559e5548
eugen0329 <eugen0329@gmail.com>
2016-05-16 10:58:00 +0300

    Add perceptron

---
+++ perceptron/main.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <vector>
+
+#include <clocale>
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
